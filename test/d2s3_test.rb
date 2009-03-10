32a6d9ec3823b9de73966c107e6f4d9e3492a0b3
mwilliams <matthew.d.williams@gmail.com>
2009-03-10 13:16:26 -0400

    Cleanup

--- test/d2s3_test.rb
+++ test/d2s3_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class D2s3Test < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
+  test "s3_http_upload_tag generates a correct form" do
     assert true
   end
 end
